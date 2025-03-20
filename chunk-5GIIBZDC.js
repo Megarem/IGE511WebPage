7b76f121191690267207055980f8e18b3d849220
Aymeric <roua4409@usherbrooke.ca>
2025-03-19 20:54:45 -0400

    feat: minimify + uglify

---
+++ chunk-5GIIBZDC.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-LDZPIT37.js";import"./chunk-WDF5FPCE.js";export{a as ChangelogComponent};
--- chunk-LGOSN3LO.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-6U5BYSSE.js";import"./chunk-A6QNABON.js";export{a as ChangelogComponent};
